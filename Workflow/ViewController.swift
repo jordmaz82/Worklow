3bb380f49caf215dde9b264ceb7845d1d24570ab
Jordan Mazurke <jordan.mazurke@cognizant.com>
2019-02-20 18:32:58 +1100

    Initial Commit

---
+++ Workflow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Workflow
+//
+//  Created by Mazurke, Jordan (Cognizant) on 20/2/19.
+//  Copyright © 2019 Mazurke, Jordan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
